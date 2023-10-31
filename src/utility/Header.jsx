8227cfc1528b8298b7fb51fd7d961d208ff7c5b3
hparekh <mail.hparekh@gmail.com>
2023-10-30 22:26:06 -0500

    Header Updated

--- src/utility/Header.jsx
+++ src/utility/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className="pt-3 py-2 pl-2" style={{ backgroundColor: "black" }}>
       <img src={logo} style={{ height: "35px", verticalAlign: "top" }}></img>{" "}
       &nbsp;
-      <span className="h2 pt-4 text-white-50">Watch-O-Pedia Hooks</span>
+      <span className="h2 pt-4 text-white-50">Watch-O-Pedia Hooks | Hooks - useState </span>
     </div>
   );
 };
